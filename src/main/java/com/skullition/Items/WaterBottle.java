d367933101111cc8cbb130464231403855f8cc50
skullition <78258797+Skullition@users.noreply.github.com>
2021-08-19 13:38:29 +0700

    add water bottle and update forge to x.x.40

---
+++ src/main/java/com/skullition/Items/WaterBottle.java
@@ -0,0 +1,10 @@
+package com.skullition.Items;
+
+import net.minecraft.world.item.Item;
+
+public class WaterBottle extends Item {
+
+    public WaterBottle(Properties properties) {
+        super(properties);
+    }
+}
