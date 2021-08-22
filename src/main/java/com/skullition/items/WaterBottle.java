92bd2611b1a414c7971dab8971060bfa46682384
skullition <78258797+Skullition@users.noreply.github.com>
2021-08-22 15:02:41 +0700

    show message when player right clicks water bottle

--- src/main/java/com/skullition/items/WaterBottle.java
+++ src/main/java/com/skullition/items/WaterBottle.java
@@ -1,7 +1,12 @@
 package com.skullition.items;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.Util;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -19,8 +24,21 @@ public class WaterBottle extends Item {
     @Override
     public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag tooltipFlag) {
         super.appendHoverText(itemStack, level, components, tooltipFlag);
-        components.add(new TranslatableComponent("tooltip.waterbottle"));
-        components.add(new TranslatableComponent("chargelevel.waterbottle", Integer.toString(5)));
+        components.add(new TranslatableComponent("tooltip.waterbottle").withStyle(ChatFormatting.BLUE));
+        int distance = itemStack.hasTag() ? itemStack.getTag().getInt("distance") : 0;
+        components.add(new TranslatableComponent("chargelevel.waterbottle", Integer.toString(distance)).withStyle(ChatFormatting.RED));
     }
 
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
+        ItemStack itemStack = player.getItemInHand(interactionHand);
+        int distance = itemStack.getOrCreateTag().getInt("distance");
+        distance++;
+        itemStack.getTag().putInt("distance", distance);
+
+        if (level.isClientSide()) {
+            player.sendMessage(new TranslatableComponent("increaseamount.waterbottle", Integer.toString(distance)), Util.NIL_UUID);
+        }
+        return super.use(level, player, interactionHand);
+    }
 }
