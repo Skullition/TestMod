4412dcadb4e2bbc2d8a6392a57d160e6f39be16f
skullition <78258797+Skullition@users.noreply.github.com>
2021-08-22 16:22:48 +0700

    Limit water bottle distance and make water bottle usable

--- src/main/java/com/skullition/items/WaterBottle.java
+++ src/main/java/com/skullition/items/WaterBottle.java
@@ -7,18 +7,16 @@ import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 
 import javax.annotation.Nullable;
 import java.util.List;
 
-public class WaterBottle extends Item {
+public class WaterBottle extends PickaxeItem {
 
     public WaterBottle(Properties properties) {
-        super(properties);
+        super(Tiers.NETHERITE, 1, -2.8F, properties);
     }
 
     @Override
@@ -34,6 +32,9 @@ public class WaterBottle extends Item {
         ItemStack itemStack = player.getItemInHand(interactionHand);
         int distance = itemStack.getOrCreateTag().getInt("distance");
         distance++;
+        if (distance >= 5) {
+            distance = 5;
+        }
         itemStack.getTag().putInt("distance", distance);
 
         if (level.isClientSide()) {
