01e22dfe34f2a44e1efea62cefa5fb585dd4aa96
skullition <78258797+Skullition@users.noreply.github.com>
2021-08-22 11:55:10 +0700

    add tooltip to waterbottle

--- src/main/java/com/skullition/items/WaterBottle.java
+++ src/main/java/com/skullition/items/WaterBottle.java
@@ -1,10 +1,26 @@
 package com.skullition.items;
 
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class WaterBottle extends Item {
 
     public WaterBottle(Properties properties) {
         super(properties);
     }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag tooltipFlag) {
+        super.appendHoverText(itemStack, level, components, tooltipFlag);
+        components.add(new TranslatableComponent("tooltip.waterbottle"));
+        components.add(new TranslatableComponent("chargelevel.waterbottle", Integer.toString(5)));
+    }
+
 }
